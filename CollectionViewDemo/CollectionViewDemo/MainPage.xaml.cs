ab99a2b761e5fafcef5bd9b210a4ba5d9e090b90
Denis Gudiel <denis20gudiel@hotmail.com>
2021-03-23 14:38:32 -0600

    Bug No bounce ios 12

---
+++ CollectionViewDemo/CollectionViewDemo/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using CollectionViewDemo.Models;
+using CollectionViewDemo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CollectionViewDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            BindingContext = new MainPageViewModels();
+        }
+
+        private void MyCollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            var products = e.CurrentSelection;            
+            string msg = String.Empty;            
+            msg = "Producto Seleccionado: \n";
+            for (int i = 0; i < products.Count; i++)
+            {
+                var product = products[i] as Product;
+                msg += $"{product.Name} Q{product.Price}";
+
+            }
+            if (e.CurrentSelection.Count == 0)            
+                return;
+                var item = e.CurrentSelection[0];
+                //perform action on selection or navigate to details view
+
+                ((CollectionView)sender).SelectedItem = null;
+                // MyCollectionView.SelectedItem = null;
+                DisplayAlert("Producto", msg, "OK");
+               
+
+
+
+        }
+
+       
+    }
+}
---
+++ CollectionViewDemo/CollectionViewDemo/ViewModels/MainPageViewModels.cs
@@ -0,0 +1,371 @@
+﻿using CollectionViewDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace CollectionViewDemo.ViewModels
+{
+    class MainPageViewModels
+    {
+        public ObservableCollection<Product> Products { get; set; }
+
+        public MainPageViewModels()
+        {
+            Products = new ObservableCollection<Product>
+            {
+                new Product
+                {
+                    Name = "Yogurt",
+                    Price = 60.0m,
+                    Image = "yogurt.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Watermelon",
+                    Price = 30.0m,
+                    Image = "watermelon.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Water Bottle",
+                    Price = 80.0m,
+                    Image = "water_bottle.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Tomato",
+                    Price = 120.0m,
+                    Image = "tomato.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Tea",
+                    Price = 65.0m,
+                    Image = "tea_bag.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Sparkling Drink",
+                    Price = 35.0m,
+                    Image = "sparkling_drink.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Spaguetti",
+                    Price = 15.0m,
+                    Image = "spaguetti.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Cream",
+                    Price = 48.0m,
+                    Image = "cream.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Snack",
+                    Price = 25.0m,
+                    Image = "snack.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Shrimp",
+                    Price = 300.0m,
+                    Image = "shrimp.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Seasoning",
+                    Price = 185.0m,
+                    Image = "seasoning.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Sauce",
+                    Price = 220.0m,
+                    Image = "sauce.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Rice",
+                    Price = 48.0m,
+                    Image = "rice.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Peas",
+                    Price = 114.0m,
+                    Image = "peas.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Ham",
+                    Price = 215.0m,
+                    Image = "ham_1.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Chicken Leg",
+                    Price = 142.0m,
+                    Image = "chicken_leg.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Pizza",
+                    Price = 321.0m,
+                    Image = "pizza.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Pineapple",
+                    Price = 49.0m,
+                    Image = "pineapple.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Pepper",
+                    Price = 60.0m,
+                    Image = "pepper.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Pasta",
+                    Price = 52.0m,
+                    Image = "pasta.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Oil Bottle",
+                    Price = 152.0m,
+                    Image = "oil_bottle",
+                    
+                },
+                new Product
+                {
+                    Name = "Mushroom",
+                    Price = 28.0m,
+                    Image = "mushroom.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Milk Bottle",
+                    Price = 85.0m,
+                    Image = "milk_bottle.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Meat",
+                    Price = 450.0m,
+                    Image = "meat.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Lemon",
+                    Price = 20.0m,
+                    Image = "lemon.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Tomato Sauce",
+                    Price = 15.0m,
+                    Image = "tomato_sauce.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Juice",
+                    Price = 60.0m,
+                    Image = "juice.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Ice Cream",
+                    Price = 251.0m,
+                    Image = "ice_cream.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Ham",
+                    Price = 290.0m,
+                    Image = "ham.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Ice",
+                    Price = 125.0m,
+                    Image = "ice.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Flour",
+                    Price = 86.0m,
+                    Image = "flour.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Fish",
+                    Price = 440.0m,
+                    Image = "fish_1.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Fish 2",
+                    Price = 425.0m,
+                    Image = "fish.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Eggs",
+                    Price = 150.0m,
+                    Image = "eggs.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Cucumber",
+                    Price = 35.0m,
+                    Image = "cucumber.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Croissant",
+                    Price = 68.0m,
+                    Image = "croissant.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Cookies",
+                    Price = 95.0m,
+                    Image = "cookie.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Coffee",
+                    Price = 154.0m,
+                    Image = "toffee.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Chocolate Bar",
+                    Price = 32.0m,
+                    Image = "chocolate_bar.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Cheese",
+                    Price = 36.0m,
+                    Image = "cheese.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Carrot",
+                    Price = 15.0m,
+                    Image = "carrot.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Canned Food",
+                    Price = 89.0m,
+                    Image = "canned_food.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Soda",
+                    Price = 45.0m,
+                    Image = "can.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Candies",
+                    Price = 55.0m,
+                    Image = "candy.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Cake",
+                    Price = 250.0m,
+                    Image = "cake.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Bread",
+                    Price = 100.0m,
+                    Image = "bread_1.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Bread",
+                    Price = 85.0m,
+                    Image = "bread.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Banana",
+                    Price = 15.0m,
+                    Image = "banana.png",
+                    
+                },
+                new Product
+                {
+                    Name = "Apple",
+                    Price = 40.0m,
+                    Image = "apple.png",
+                   
+                },
+                new Product
+                {
+                    Name = "Alcohol",
+                    Price = 370.0m,
+                    Image = "apple.png",
+                    
+                },
+            };
+
+        }
+    }
+}
